a015422d0273f3e418fe099bb11291cce9a51b47
nunya biz <nunya@bizniz.con>
2020-09-20 19:58:01 -0400

    Add Ribbon annotation for connection to CurrencyExchangeService

--- src/main/java/com/katgrennan/currencyexchange/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/katgrennan/currencyexchange/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,14 @@
 package com.katgrennan.currencyexchange.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// Interface for currency-exchange-service
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+// Feign proxy interface for currency-exchange-service
+@FeignClient(name = "currency-exchange-service")
+// Ribbon for client-side load-balancing
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     // Method signature from currency-exchange-service
