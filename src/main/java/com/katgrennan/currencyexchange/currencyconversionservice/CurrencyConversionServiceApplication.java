9c448653b41a86f4be6b329b6accf6476b046c77
nunya biz <nunya@bizniz.con>
2020-09-20 21:31:58 -0400

    Enable discovery for Eureka naming server

--- src/main/java/com/katgrennan/currencyexchange/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/katgrennan/currencyexchange/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,14 @@ package com.katgrennan.currencyexchange.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+// Enable Feign client for this service
 @EnableFeignClients("com.katgrennan.currencyexchange.currencyconversionservice")
+// Enable discovery for Eureka naming server
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
