8597746080d825973c258b5113aa7b4355da5ba4
nunya biz <nunya@bizniz.con>
2020-09-26 20:37:13 -0400

    Add Sampler to catch all logs

--- src/main/java/com/katgrennan/currencyexchange/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/katgrennan/currencyexchange/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,9 +1,11 @@
 package com.katgrennan.currencyexchange.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 // Enable Feign client for this service
@@ -14,6 +16,11 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
